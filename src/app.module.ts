b31ba8ec2b807d38bcff1511d1e3ae9c66aa5fa0
Alvaro Alarcon Reynaga <alvaro.alarcon@ucb.edu.bo>
2026-01-26 23:21:35 -0400

    feat: add health modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from "./prisma/prisma.module";
 import { ProductsModule } from "./products/products.module";
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
