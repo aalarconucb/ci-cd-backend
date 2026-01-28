f99c3fd2a3f070fcc0e385aa57593ab59b4e660d
aalarconucb <alvaro.alarcon@ucb.edu.bo>
2026-01-28 16:33:36 -0400

    feat: add env config with @nestjs/config (#4)
    * feat: add env config with @nestjs/config
    
    * style: fix prettier formatting
    
    * test: silence unsafe-argument for supertest server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,7 +7,15 @@ import { ProductsModule } from './products/products.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local'],
+    }),
+    PrismaModule,
+    ProductsModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
